7088d887bbd7ae13ecc8487bf5a685010cc02989
disciplinedHalfAss <randy.y.yuan@gmail.com>
2016-05-31 22:12:15 -0700

    Changed menu button to hamburger button and added transition effect

--- js/index.js
+++ js/index.js
@@ -2,9 +2,13 @@ var menuOpened = false;
 $('.menu-control').click(function(event){
 	if(menuOpened ==false){
 		$('.body').addClass('menu-open');
+		$('.fa').removeClass('fa-bars');
+		$('.fa').addClass('fa-times');
 		menuOpened = true;
 	}else{
 		$('.body').removeClass('menu-open');
+		$('.fa').removeClass('fa-times');
+		$('.fa').addClass('fa-bars');
 		menuOpened = false;
 	}
 });

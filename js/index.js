9b53020075d6694aa22e6616ea8b6ab660fb627b
disciplinedHalfAss <randy.y.yuan@gmail.com>
2016-06-01 09:35:50 -0700

    Added parallax effect and debug button for parallax effect

--- js/index.js
+++ js/index.js
@@ -1,22 +1,32 @@
 // var mainbottom = $('#main').offset().top + $('#main').height();
 var menuOpened = false;
-
+var debugMode = false;
 
 $('.menu-control').click(function(event){
 	if(menuOpened ==false){
-		$('.body').addClass('menu-open');
+		// $('.body').addClass('menu-open');
+		$('.side-menu').addClass('side-menu-appear');
 		$('.fa').removeClass('fa-bars');
 		$('.fa').addClass('fa-times');
 		menuOpened = true;
 	}else{
-		$('.body').removeClass('menu-open');
+		// $('.body').removeClass('menu-open');
+		$('.side-menu').removeClass('side-menu-appear');
 		$('.fa').removeClass('fa-times');
 		$('.fa').addClass('fa-bars');
 		menuOpened = false;
 	}
 });
-
-
+$('.debug').click(function(event){
+	if(debugMode==false){
+		$('.all').addClass('transform');
+		debugMode=true;
+	}else
+	{
+		$('.all').removeClass('transform');
+		debugMode=false;
+	}
+});
 // $(window).on('scroll',function(){
 //     var stop = Math.round($(window).scrollTop());
 

1a91ebcec0839971c26f907a59fb81598a5401ff
disciplinedHalfAss <randy.y.yuan@gmail.com>
2016-06-02 20:52:36 -0700

    Added nav bar animation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// var mainbottom = $('#main').offset().top + $('#main').height();
 var menuOpened = false;
 var debugMode = false;
 
@@ -29,15 +28,16 @@ $('.debug').click(function(event){
 });
 
 for(var i=0; i<1;i++){
-$('.parallax__layer--back').append('<div class="shape-map"><svg class="shape triangle"><polygon class="color" points="0,0 50,87 100,0"/></svg><svg class="shape circle-one"><circle class="color" cx="50" cy="50" r="20"></svg><svg class="shape c-two"><circle class="color" cx="50" cy="50" r="20"></svg><svg class="shape square"><polygon class="color" points="40,40 60,40 60,60 40,60"/></svg><svg class="shape hexagon"><polygon class="color" points="27,7 73,7 100,50 73,93 27,93 0,50"/></svg><svg class="shape arrow"><polygon class="color" points="50,0 30,80 50,0 70,80"/></svg></div>');
+$('.parallax__layer--back').append('<div class="shape-map"><svg class="shape triangle"><polygon class="color" points="0,0 50,87 100,0"/></svg><svg class="shape circle-one"><circle class="color" cx="50" cy="50" r="20"></svg><svg class="shape c-two"><circle class="color" cx="50" cy="50" r="20"></svg><svg class="shape square"><polygon class="color" points="40,40 60,40 60,60 40,60"/></svg><svg class="shape hexagon"><polygon class="color" points="27,7 73,7 100,50 73,93 27,93 0,50"/></svg><svg class="shape arrow"><polygon class="color" points="18,0 100,60 0,60 82,0 50,100"/></svg></div>');
 }
-// $(window).on('scroll',function(){
-//     var stop = Math.round($(window).scrollTop());
 
-//     if (stop > mainbottom) {
-//         $('').addClass('past-main');
-//     } else {
-//         $('.nav').removeClass('past-main');
-//     }
 
-// });
+var mainbottom = $('.head-bar').offset().top + $('.head-bar').height();
+$('.parallax').scroll(function() {
+    var stop = Math.round($('.parallax').scrollTop());
+    if (stop > mainbottom) {
+        $('.nav').addClass('past-main');
+    } else {
+        $('.nav').removeClass('past-main');
+    }
+});
